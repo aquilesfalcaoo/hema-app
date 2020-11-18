7d9fd0556077bb02b9bf40e1ad3e78ec05d3711e
Aquiles Falcão <aquilesfalcao@hotmail.com>
2020-11-18 12:43:10 -0300

    integrado o back-end ao front

--- doacaodesangue/views.py
+++ doacaodesangue/views.py
@@ -1,39 +1,37 @@
 from django.shortcuts import render, redirect
 from .models import Doador
 from .forms import DoadorForm
-from .models import Medico
-from .forms import MedicoForm
 
-def list_donor(request):
-    donors = Doador.objects.all()
-    return render(request, 'donor.html', {'donors': donors})
+def list_doador(request):
+    doador = Doador.objects.all()
+    return render(request, 'doador.html', {'doador': doador})
 
-def create_donor(request):
+def create_doador(request):
     form = DoadorForm(request.POST or None)
 
     if form.is_valid():
         form.save()
-        return redirect('list_donor')
+        return redirect('list_doador')
 
-    return render(request, 'donor-form.html', {'form': form})
+    return render(request, 'doador-form.html', {'form': form})
 
 
-def update_donor(request, id):
-    donors = Doador.objects.get(id=id)
-    form = DoadorForm(request.POST or None, instance=donors)
+def update_doador(request, id):
+    doador = Doador.objects.get(id=id)
+    form = DoadorForm(request.POST or None, instance=doador)
 
     if form.is_valid():
         form.save()
-        return redirect('list_donor')
+        return redirect('list_doador')
 
-    return render(request, 'donor-form.html', {'form': form, 'donors': donors})
+    return render(request, 'doador-form.html', {'form': form, 'doador': doador})
 
 
-def delete_crime(request, id):
-    donors = Doador.objects.get(id=id)
+def delete_doador(request, id):
+    doador = Doador.objects.get(id=id)
 
     if request.method == 'POST':
-        donors.delete()
-        return redirect('list_donor')
+        doador.delete()
+        return redirect('list_doador')
 
-    return render(request, 'donor-delete-confirm.html', {'donors': donors})
+    return render(request, 'doador-delete-confirm.html', {'doador': doador})
