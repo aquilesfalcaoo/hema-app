a9b46df5983252114ef26289d27dba7d1a9a5f2a
Aquiles Falcão <aquilesfalcao@hotmail.com>
2020-11-25 12:27:01 -0300

    adicionado bootstrap forms

--- perfil/settings.py
+++ perfil/settings.py
@@ -42,7 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'doacaodesangue',
     'perfil',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
